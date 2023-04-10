a956972b532455a6e29dc0043106cdf50007d6d1
genereodan22 <125712253+genereodan22@users.noreply.github.com>
2023-04-10 22:22:04 +0800

    modified:   application.js 	modified:   index.html

--- application.js
+++ application.js
@@ -2,6 +2,7 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
   import { getAuth,createUserWithEmailAndPassword, } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+  import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
